d883803ad6e6f6f520e6f836cd515ec011f8055d
Sanmitra11 <sanfriend11@gmail.com>
2025-03-01 15:35:29 +0530

    initial commit

--- public/js/map.js
+++ public/js/map.js
@@ -5,7 +5,7 @@
   const map = new mapboxgl.Map({
       container: 'map', // container ID
       style: "mapbox://styles/mapbox/streets-v12",
-      center: [ 73.856285, 18.515669], // Ensure this is an array [lng, lat]
+      center: coordinates, // Ensure this is an array [lng, lat]
       zoom: 9
   });
      
